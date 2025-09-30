4329f5b7b1d0aec78143c039e960439acf15ba1c
Pedro S <pedrosilva_02@outlook.com>
2025-09-30 23:42:30 +0100

    [utils] made type erasure easier to use and gave it better crash error messages

--- include/mars/meta/type_erasure.hpp
+++ include/mars/meta/type_erasure.hpp
@@ -6,35 +6,31 @@
 
 #include <cstddef>
 #include <meta>
+#include <type_traits>
 
 namespace mars::meta {
     namespace detail {
-        struct type_id;
-
         inline static log_channel type_erasure_channel("mars/meta/type_erasure");
-
-        consteval {
-            if (!environment::is_shipping) {
-                std::meta::define_aggregate(^^type_id, {
-                                                           std::meta::data_member_spec(^^size_t, {
-                                                                                                     .name = "hash" }) });
-            } else
-                std::meta::define_aggregate(^^type_id, {
-                                                       });
-        };
     } // namespace detail
 
     struct type_erasure_ptr {
       private:
+        struct empty {};
+
         void* data = nullptr;
 
         [[no_unique_address]]
-        detail::type_id id;
+        std::conditional_t<environment::is_shipping, empty, void (*)(const std::string_view&)> id;
+
+        template <typename T>
+        static void internal_error(const std::string_view& _value) {
+            logger::assert_(detail::type_erasure_channel, "attempted to get type {} but {} is stored", _value, std::meta::display_string_of(^^T));
+        }
 
       public:
         type_erasure_ptr() {
             if (!environment::is_shipping)
-                id = { 0 };
+                id = nullptr;
         }
 
         template <typename T>
@@ -47,15 +43,15 @@ namespace mars::meta {
             data = _ptr;
 
             if (!environment::is_shipping)
-                id.hash = hash::type_fingerprint_v<T>;
+                id = &internal_error<T>;
         }
 
         template <typename T>
         T* get() const {
             if (!environment::is_shipping) {
-                if (id.hash == 0 || id.hash == hash::type_fingerprint_v<T>)
+                if (id == nullptr || id == &internal_error<T>)
                     return static_cast<T*>(data);
-                logger::assert_(detail::type_erasure_channel, "attempted to get type {} but hash is {}", std::meta::identifier_of(^^T), id.hash);
+                id(std::meta::display_string_of(^^T));
                 return nullptr;
             }
             return static_cast<T*>(data);
@@ -70,6 +66,11 @@ namespace mars::meta {
         type_erasure_ptr& operator=(T* _ptr) {
             store(_ptr);
             return *this;
+        }
+
+        template <typename T>
+        operator T*() const {
+            return get<T>();
         }
     };
 } // namespace mars::meta
